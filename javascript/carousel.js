bf5569741e852303517f719f09bc2c81b057e2d2
Macky184 <taiki0306090609@icloud.com>
2025-12-26 16:34:39 +0900

    修正(2025/12/26)

---
+++ javascript/carousel.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("LOADED");
+    const track = document.querySelector(".carousel-track");
+    const slides = Array.from(track.children);
+    let index = 0;
+
+    function update(){
+        track.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    document.querySelector(".next").addEventListener("click", () => {
+        console.log(1);
+        index = (index + 1) % slides.length;
+        update();
+    });
+
+    document.querySelector(".prev").addEventListener("click", () => {
+        index = (index - 1 + slides.length) % slides.length;
+        update();
+    });
+
+});
+
